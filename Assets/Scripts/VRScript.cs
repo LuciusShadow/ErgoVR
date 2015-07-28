1627e81544bd96365cba114268a8ead9d81ab983
Sascha <sascha.bach@d120.de>
2015-07-28 13:01:22 +0200

    Unity Integration

---
+++ Assets/Scripts/VRScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using VR = UnityEngine.VR;
+
+public class VRScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(OVRManager.display.acceleration);
+	}
+}
